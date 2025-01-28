301871efcb65e64c6febbe2b601759d1855dc83d
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2025-01-28 21:37:09 +0700

    menjalankan aplikasi

---
+++ app/src/main/java/com/example/projectfilm_135/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.projectfilm_135
+
+import PengelolaHalamanFilm
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectfilm_135.ui.theme.ProjectFilm_135Theme
+import com.example.projectfilm_135.ui.view.film.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjectFilm_135Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PengelolaHalamanFilm(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjectFilm_135Theme {
+        Greeting("Android")
+    }
+}
